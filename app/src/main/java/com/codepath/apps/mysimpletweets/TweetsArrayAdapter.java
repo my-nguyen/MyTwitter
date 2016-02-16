992ab6f886cb34a224a26a69401f4b5fcfe60692
iceguyvn <nguyen_my@yahoo.com>
2016-02-16 15:27:31 -0800

    Displayed the relative timestamp for each tweet

--- app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
@@ -1,6 +1,8 @@
 package com.codepath.apps.mysimpletweets;
 
 import android.content.Context;
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +13,11 @@ import android.widget.TextView;
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by My on 2/16/2016.
@@ -35,15 +41,47 @@ import java.util.List;
       ImageView profileImage = (ImageView)convertView.findViewById(R.id.profile_image);
       TextView name = (TextView)convertView.findViewById(R.id.name);
       TextView screenName = (TextView)convertView.findViewById(R.id.screen_name);
+      TextView timeAgo = (TextView)convertView.findViewById(R.id.time_ago);
       TextView body = (TextView)convertView.findViewById(R.id.body);
       // populate data into the subviews
       name.setText(tweet.getUser().getName());
       screenName.setText("@" + tweet.getUser().getScreenName());
+      timeAgo.setText(abbreviate(getRelativeTimeAgo(tweet.getCreatedAt())));
       body.setText(tweet.getBody());
       // clear out the old image for a recycled view
       profileImage.setImageResource(android.R.color.transparent);
       Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(profileImage);
       // return the view to be inserted into the list
       return convertView;
+   }
+
+   // this method returns an abbreviated "time ago" string in the format of 20m, 2h, etc.
+   private String abbreviate(String relativeTime) {
+      Log.d("NGUYEN", "relativeTime: " + relativeTime);
+      // split string into tokens
+      String[] tokens = relativeTime.toString().split(" ");
+      // return number appended with "m" or "h", e.g. 20m, 2h
+      return tokens[0] + tokens[1].charAt(0);
+   }
+
+   // this method is supplied by Nathan Esquizi. it takes a raw Json date string in the format of
+   // "Tue Feb 16 23:22:46 +0000 2016" and returns that time in the "ago" format: 3 minutes ago,
+   // 53 seconds ago, etc.
+   private String getRelativeTimeAgo(String rawJsonDate) {
+      String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+      SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+      sf.setLenient(true);
+
+      String relativeDate = "";
+      try {
+         Date date = sf.parse(rawJsonDate);
+         long dateMillis = date.getTime();
+         relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+               System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+      } catch (ParseException e) {
+         e.printStackTrace();
+      }
+
+      return relativeDate;
    }
 }
