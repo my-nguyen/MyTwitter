51b7008ff141d83f7d4bb1e3a62952bb5d516684
iceguyvn <nguyen_my@yahoo.com>
2016-02-17 16:00:08 -0800

    Added in ComposeActivity a character count TextView next to the "Tweet" button; also enable the Cancel ImageButton (top right)

--- app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
@@ -2,12 +2,18 @@ package com.codepath.apps.mysimpletweets;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -35,14 +41,54 @@ public class ComposeActivity extends AppCompatActivity {
       ImageView profileImage = (ImageView)findViewById(R.id.profile_image);
       TextView name = (TextView)findViewById(R.id.name);
       TextView screenName = (TextView)findViewById(R.id.screen_name);
-      final TextView text = (TextView)findViewById(R.id.text);
-      Button tweetButton = (Button)findViewById(R.id.tweet_button);
+      ImageButton cancelButton = (ImageButton)findViewById(R.id.cancel_button);
+      final EditText text = (EditText)findViewById(R.id.text);
+      final TextView textCount = (TextView)findViewById(R.id.text_count);
+      final Button tweetButton = (Button)findViewById(R.id.tweet_button);
 
       // populate data into the subviews
       profileImage.setImageResource(android.R.color.transparent);
       Picasso.with(this).load(user.getProfileImageUrl()).into(profileImage);
       name.setText(user.getName());
       screenName.setText("@" + user.getScreenName());
+      cancelButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            finish();
+         }
+      });
+      text.addTextChangedListener(new TextWatcher() {
+         ColorStateList mNormalColors = null;
+         @Override
+         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            if (mNormalColors == null)
+               mNormalColors = textCount.getTextColors();
+         }
+
+         @Override
+         public void onTextChanged(CharSequence s, int start, int before, int count) {
+         }
+
+         @Override
+         public void afterTextChanged(Editable s) {
+            // the number of characters in the EditText
+            int count = s.toString().length();
+            // update text with the number
+            textCount.setText(Integer.toString(count));
+            if (count > 140) {
+               // change text color to RED
+               textCount.setTextColor(Color.RED);
+               // disable the "Tweet" button
+               tweetButton.setEnabled(false);
+            } else {
+               // restore color
+               textCount.setTextColor(mNormalColors);
+               // enable the "Tweet" button
+               tweetButton.setEnabled(true);
+            }
+         }
+      });
+
       tweetButton.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
@@ -52,13 +98,16 @@ public class ComposeActivity extends AppCompatActivity {
                mClient.postStatus(status, new JsonHttpResponseHandler() {
                   @Override
                   public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                     // compose a Tweet from the text and User data
                      Tweet tweet = new Tweet();
                      tweet.text = status;
                      tweet.user = user;
                      tweet.createdAt = new Date().toString();
+                     // stuff that Tweet in an Intent, to pass back to TimelineActivity
                      Intent data = new Intent();
                      data.putExtra("TWEET_OUT", tweet);
                      setResult(RESULT_OK, data);
+                     // dismiss the ComposeActivity screen
                      finish();
                   }
                });
