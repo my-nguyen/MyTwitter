fa227ee4745140fd3aa31dcee5027853959b5e3f
iceguyvn <nguyen_my@yahoo.com>
2016-02-22 22:25:55 -0800

    Applied ButterKnife to TweetsArrayAdapter

--- app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TweetsArrayAdapter.java
@@ -19,11 +19,29 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by My on 2/16/2016.
  */
 // this class takes the Tweet objects and turns them into Views to be displayed in the ListView
    public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
+   static class ViewHolder {
+      @Bind(R.id.profile_image)  ImageView         profileImage;
+      @Bind(R.id.name)           TextView          name;
+      @Bind(R.id.screen_name)    TextView          screenName;
+      @Bind(R.id.time_ago)       TextView          timeAgo;
+      @Bind(R.id.text)           LinkifiedTextView text;
+      @Bind(R.id.image)          ImageView         image;
+      @Bind(R.id.retweet_count)  TextView          retweetCount;
+      @Bind(R.id.favorite_count) TextView          favoriteCount;
+
+      public ViewHolder(View view) {
+         ButterKnife.bind(this, view);
+      }
+   }
+
    public TweetsArrayAdapter(Context context, List<Tweet> tweets) {
       // super(context, android.R.layout.simple_list_item_1, tweets);
       super(context, 0, tweets);
@@ -34,34 +52,31 @@ import java.util.Locale;
    public View getView(int position, View convertView, ViewGroup parent) {
       // get the tweet
       Tweet tweet = getItem(position);
+      ViewHolder holder;
       // find or inflate the template
-      if (convertView == null)
+      if (convertView == null) {
          convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
-      // find the subviews to fill with data in the template
-      ImageView profileImage = (ImageView)convertView.findViewById(R.id.profile_image);
-      TextView name = (TextView)convertView.findViewById(R.id.name);
-      TextView screenName = (TextView)convertView.findViewById(R.id.screen_name);
-      TextView timeAgo = (TextView)convertView.findViewById(R.id.time_ago);
-      LinkifiedTextView text = (LinkifiedTextView)convertView.findViewById(R.id.text);
-      ImageView image = (ImageView)convertView.findViewById(R.id.image);
-      TextView retweetCount = (TextView)convertView.findViewById(R.id.retweet_count);
-      TextView favoriteCount = (TextView)convertView.findViewById(R.id.favorite_count);
+         holder = new ViewHolder(convertView);
+         convertView.setTag(holder);
+      }
+      else
+         holder = (ViewHolder)convertView.getTag();
       // populate data into the subviews
       // clear out the old image for a recycled view
-      profileImage.setImageResource(android.R.color.transparent);
-      Picasso.with(getContext()).load(tweet.user.profileImageUrl).into(profileImage);
-      name.setText(tweet.user.name);
-      screenName.setText("@" + tweet.user.screenName);
-      timeAgo.setText(abbreviate(getRelativeTimeAgo(tweet.createdAt)));
-      text.setText(tweet.text);
+      holder.profileImage.setImageResource(android.R.color.transparent);
+      Picasso.with(getContext()).load(tweet.user.profileImageUrl).into(holder.profileImage);
+      holder.name.setText(tweet.user.name);
+      holder.screenName.setText("@" + tweet.user.screenName);
+      holder.timeAgo.setText(abbreviate(getRelativeTimeAgo(tweet.createdAt)));
+      holder.text.setText(tweet.text);
       if (tweet.mediaUrl != null) {
          int widthPixels = getContext().getResources().getDisplayMetrics().widthPixels;
-         Picasso.with(getContext()).load(tweet.mediaUrl).resize(widthPixels, 0).into(image);
+         Picasso.with(getContext()).load(tweet.mediaUrl).resize(widthPixels, 0).into(holder.image);
       }
       if (tweet.retweetCount > 0)
-         retweetCount.setText(Integer.toString(tweet.retweetCount));
+         holder.retweetCount.setText(Integer.toString(tweet.retweetCount));
       if (tweet.favoriteCount > 0)
-         favoriteCount.setText(Integer.toString(tweet.favoriteCount));
+         holder.favoriteCount.setText(Integer.toString(tweet.favoriteCount));
       // return the view to be inserted into the list
       return convertView;
    }
