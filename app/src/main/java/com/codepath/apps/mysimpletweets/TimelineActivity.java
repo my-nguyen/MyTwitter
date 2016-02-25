f33e46ea1f8691a2f625cd762b5dc7ca2aed6860
iceguyvn <nguyen_my@yahoo.com>
2016-02-25 10:27:28 -0800

    Preliminary transfer of logic from TimelineActivity to TweetListFragment

--- app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/TimelineActivity.java
@@ -1,89 +1,18 @@
 package com.codepath.apps.mysimpletweets;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.codepath.apps.mysimpletweets.models.Tweet;
-import com.codepath.apps.mysimpletweets.models.User;
-import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.apache.http.Header;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
-public class TimelineActivity extends AppCompatActivity implements ComposeFragment.ComposeFragmentListener {
-   private static final int   REQUEST_CODE = 100;
-   private TwitterClient            mClient;
-   private List<Tweet>              mTweets;
-   private TweetRecyclerViewAdapter mAdapter;
-   private User                     mCurrentUser = null;
-   @Bind(R.id.swipe_container)   SwipeRefreshLayout   mSwipeContainer;
-   @Bind(R.id.tweets_view)       RecyclerView         mListView;
-
+public class TimelineActivity extends AppCompatActivity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_timeline);
-
-      ButterKnife.bind(this);
-      // create an ArrayList data source
-      mTweets = new ArrayList<>();
-      // get the singleton client
-      mClient = TwitterApplication.getRestClient();
-      // fetch and save the current user's credentials, for use in composing a new Tweet
-      getUserCredentials();
-      // construct an adapter from the data source
-      mAdapter = new TweetRecyclerViewAdapter(this, mTweets, mCurrentUser);
-      // connect the adapter to the ListView
-      mListView.setAdapter(mAdapter);
-      // set layout manager to position the items
-      LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-      mListView.setLayoutManager(layoutManager);
-      // mListView.setOnScrollListener(new ListViewScrollListener() {
-      mListView.addOnScrollListener(new RecyclerViewScrollListener(layoutManager) {
-         @Override
-         public void onLoadMore(int page, int totalItemsCount) {
-            // triggered only when new data needs to be appended to the list, in this case when
-            // lowestId is not 0.
-            populateTimeline(findLowestId());
-         }
-      });
-      // add dividers (decorators) between RecyclerView items
-      RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
-      mListView.addItemDecoration(itemDecoration);
-      // populate timeline upon startup
-      populateTimeline(0);
-      // set up refresh listener
-      mSwipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-         @Override
-         public void onRefresh() {
-            // re-populate the timeline
-            populateTimeline(0);
-         }
-      });
-      // configure the refreshing colors
-      mSwipeContainer.setColorSchemeColors(
-            android.R.color.holo_blue_bright,
-            android.R.color.holo_green_light,
-            android.R.color.holo_orange_light,
-            android.R.color.holo_red_light);
    }
 
    @Override
@@ -92,14 +21,11 @@ public class TimelineActivity extends AppCompatActivity implements ComposeFragme
       return true;
    }
 
+   /*
    @Override
    public boolean onOptionsItemSelected(MenuItem item) {
       switch (item.getItemId()) {
          case R.id.compose:
-            /*
-            Intent intent = ComposeActivity.newIntent(this, mUser);
-            startActivityForResult(intent, REQUEST_CODE);
-             */
             ComposeFragment dialog = ComposeFragment.newInstance(mCurrentUser);
             dialog.show(getSupportFragmentManager(), "COMPOSE_FRAGMENT");
             return true;
@@ -127,98 +53,5 @@ public class TimelineActivity extends AppCompatActivity implements ComposeFragme
       mTweets.add(0, tweet);
       mAdapter.notifyItemInserted(0);
    }
-
-   // lowestId == 0 means this is a fresh new feed of 25 tweets
-   // lowestId != 0 means to fetch the next 25 tweets beyond the current list of tweets in the timeline
-   private void populateTimeline(final long lowestId) {
-      if (!isNetworkAvailable() || !isOnline()) {
-         Log.d("NGUYEN", "Network isn't available");
-         // mAdapter.clear();
-         int count = mAdapter.getItemCount();
-         if (count > 0) {
-            mTweets.clear();
-            mAdapter.notifyItemRangeRemoved(0, count);
-         }
-         // load tweet feed from local database instead of from twitter.com
-         List<Tweet> tweets = Tweet.getAll();
-         Log.d("NGUYEN", "fetched " + tweets.size() + " tweets from the database");
-         // mAdapter.addAll(tweets);
-         count = mAdapter.getItemCount();
-         mTweets.addAll(tweets);
-         mAdapter.notifyItemRangeInserted(count, tweets.size());
-         // signal swipe refresh has finished
-         mSwipeContainer.setRefreshing(false);
-      }
-      else {
-         // retrieve a feed of 25 tweets for the home timeline from twitter.com
-         mClient.getHomeTimeline(lowestId, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-               Log.d("NGUYEN", response.toString());
-               // clear adapter and database on a fresh new feed of tweets
-               int count = mAdapter.getItemCount();
-               if (lowestId == 0) {
-                  Tweet.deleteAll();
-                  // mAdapter.clear();
-                  if (count > 0) {
-                     mTweets.clear();
-                     mAdapter.notifyItemRangeRemoved(0, count);
-                  }
-               }
-               // create tweet objects (and save them to local database) from JSON feed from twitter.com
-               List<Tweet> tweets = Tweet.fromJSONArray(response);
-               Log.d("NGUYEN", "fetched " + tweets.size() + " tweets from twitter.com");
-               // with a load-more feed (endless scroll), just add the feed to the current list of feed
-               // mAdapter.addAll(tweets);
-               count = mAdapter.getItemCount();
-               mTweets.addAll(tweets);
-               mAdapter.notifyItemRangeInserted(count, tweets.size());
-               // signal swipe refresh has finished
-               mSwipeContainer.setRefreshing(false);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-               if (errorResponse != null)
-                  Log.d("NGUYEN", errorResponse.toString());
-            }
-         });
-      }
-   }
-
-   // this method finds the new lowest id, for subsequent fetches beyond the current 25 tweets
-   private long findLowestId() {
-      long lowest = mTweets.get(0).uid;
-      for (int i = 1; i < mTweets.size(); i++)
-         if (lowest > mTweets.get(i).uid)
-            lowest = mTweets.get(i).uid;
-      return lowest;
-   }
-
-   private void getUserCredentials() {
-      mClient.getUserCredentials(new JsonHttpResponseHandler() {
-         @Override
-         public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-            mCurrentUser = User.fromJsonObject(response);
-         }
-      });
-   }
-
-   private Boolean isNetworkAvailable() {
-      ConnectivityManager connectivityManager
-            = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-      NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-      return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
-   }
-
-   private boolean isOnline() {
-      Runtime runtime = Runtime.getRuntime();
-      try {
-         Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
-         int     exitValue = ipProcess.waitFor();
-         return (exitValue == 0);
-      } catch (IOException e)          { e.printStackTrace(); }
-      catch (InterruptedException e) { e.printStackTrace(); }
-      return false;
-   }
+      */
 }
